67d34553fb23a922cd125be8493bb58d83c7ec9f
Nifeo <beiyongju@gmail.com>
2017-09-19 17:44:05 +1200

    v0.2
    adding home button picture

--- src/component/js/Header.js
+++ src/component/js/Header.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import '../css/style_header.css';
-class Header extends React.Component{
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import hbt from "../image/hb.png";
+const sty = {height:'64px',width:'64px',marginLeft:'3%'};
+class Header extends React.Component{	
 	render(){
 		return (
+			<Router>
 			<div id="wrap_header">
-			<div className="navB">
-				<ul className="nav">
-					<li>About</li>
-					<li>Resume</li>
+			<div>
+				<ul className="nav" >
+					<li><Link to="/about">About</Link></li>
+					<li><Link to="/resume">Resume</Link></li>
 				</ul>
-				<img className="nav" alt=" " src=""></img>
+				<img className="nav" style={sty} src={hbt}></img>
 				<ul className="nav">
-					<li>Artical</li>
-					<li>Coding lab</li>
+					<li><Link to="/artical">Artical</Link></li>
+					<li><Link to="/codinglab">Coding lab</Link></li>
 				</ul>
 			</div>
+			<Route exact path="/" component={Home}/>
 			</div>
+			</Router>
 		);
 	}
 }
+
+const Home = () =>(
+	<div id="selfIntro">
+		<h1>Hello World!</h1>	
+		<p>I'm Rick, weclome to my blog.<br/>hope you enjoy and have fun :)</p>
+	</div>
+)
 export default Header;
--- src/component/main.js
+++ src/component/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './js/Header';
 import Footer from './js/Footer';
 import './css/style_main.css';
-import {BrowserRouter} from 'react-router-dom';
+//import {BrowserRouter} from 'react-router-dom';
 
 class Home extends React.Component{
 	render(){
@@ -14,8 +14,8 @@ class Home extends React.Component{
 					<div id="temp">
 						<h1>what's next</h1>
 						<ol>
-							<li>update from new folder</li>
-							<li>image!image!image!</li>
+							<li>react router</li>
+							<li></li>
 						</ol>
 					</div>
 				</div>
